728c05290bd90fa67cb980e0352310396802c16b
DeltaGoldenFlag <david.wongcascante@digipen.edu>
2020-11-05 15:23:08 -0800

    Made a template file to use for the engine's source files.

---
+++ Source/Template.cxx
@@ -0,0 +1,50 @@
+/* ========================================================================= */
+/**
+ * @author          David Wong Cascante
+ * @file            File Header
+ * @par             Ludus Engine
+ * @date            [ Date Here ]
+ * 
+ * @brief
+ * [ Write a description about what the system does and what it is in 
+ * charge of ]
+ **/
+/* ========================================================================= */
+
+/* ========================================================================= */
+#ifndef Module_Name_MODULE_H
+#define Module_Name_MODULE_H
+/* ========================================================================= */
+
+// Everyting belongs on the Ludus namespace.
+// #include <memory>
+// Use shared_ptr and unique_ptr instead of raw pointers.
+namespace Ludus
+{
+    // Define final if possible as a C++11 feature.
+    class Dummy final : public LudusObject
+    {
+    public:
+        /* ================================================================= */
+        /**
+         * [Write brief here]
+         * Delete this or doxygen will cry.
+         * @param               [Write Parameter]
+         * @returns             [Return Value Here]
+         **/
+    private:
+        /* ================================================================= */
+        /* ================================================================= */
+        /*
+         * Variable names should go as:
+         * TYPE const * const &type_;
+         * TYPE *type_;
+         * TYPE const *type_;
+         */
+        /* ================================================================= */
+    };
+}
+
+/* ========================================================================= */
+#endif // Module_Name_MODULE_H
+/* ========================================================================= */
