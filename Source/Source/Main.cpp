2fa9b7eb13d9661996fa41aabd60f18a04a8e793
DeltaGoldenFlag <david.wongcascante@digipen.edu>
2020-11-04 19:23:44 -0800

    Started trying to organize the source files for the engine.

---
+++ Source/Source/Main.cpp
@@ -0,0 +1,21 @@
+/* ========================================================================= */
+/**
+ * @author          David Wong Cascante
+ * @file            Main.cpp
+ * @par             
+ * @date            11/04/2020
+ * 
+ * @brief
+ * Provides the main entry file for the project.
+ * 
+ **/
+/* ========================================================================= */
+
+#include <iostream>
+#include "../Include/Engine.hpp"
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
