f4902de30377469c7ea29e3f51e9238bd6f2a98c
DeltaGoldenFlag <david.wongcascante@digipen.edu>
2020-11-05 20:42:11 -0800

    Got the initial setup working for Node

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -3,7 +3,7 @@
  * @author          David Wong Cascante
  * @file            Tests.cpp
  * @par             Ludus Engine
- * @date            [ Date Here ]
+ * @date            11/05/2020
  * 
  * @brief
  * Runs all the tests for the entire engine???
@@ -17,13 +17,14 @@
 #include <memory>
 #include "Ludus/Precompile.hpp"
 #define CATCH_CONFIG_MAIN
-#include "Vendor/catch2.hpp"
+#include "catch2.hpp"
 
 #include "Ludus/System/Node.hpp"
 
-TEST_CASE("Testing simple parent-child relationships")
+TEST_CASE("Testing simple parent-child relationships", "[Node]")
 {
     using Ludus::Node;
     std::unique_ptr<Node> node = std::make_unique<Node>("Parent");
-    node->AddChild(std::make_unique<Node>("Child"));
+    std::unique_ptr<Node>child = std::make_unique<Node>("Child");
+    node->AddChild(child);
 }
